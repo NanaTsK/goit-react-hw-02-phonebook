ad2ebd908977e98dff0f08c3bee6f2a960cace4c
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-10 15:48:53 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,18 +22,11 @@ export class App extends Component {
 
   // state = {
   //   contacts: [
-  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  //     { id: 'id-1', name: 'Lesya Ukrainka', number: '459-12-56' },
+  //     { id: 'id-2', name: 'Boris JohnsonUK', number: '443-89-12' },
+  //     { id: 'id-3', name: 'Taras Shevchenko', number: '645-17-79' },
+  //     { id: 'id-4', name: 'Crimea BeachClub', number: '227-91-26' },
   //   ],
-  //   filter: '',
-  //   name: '',
-  //   number: '',
-  // };
-
-  // state = {
-  //   contacts: [],
   //   filter: '',
   // };
 
