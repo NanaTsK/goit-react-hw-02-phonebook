c8b93ae47cde3c888bcd4431de2113670eee8270
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-10 22:41:42 +0200

    contact form

--- src/components/ContactForm/ContactForm.styled.js
+++ src/components/ContactForm/ContactForm.styled.js
@@ -5,9 +5,37 @@ export const PhonebookForm = styled('form')({
   flexDirection: 'column',
   paddingBottom: '15px',
 });
-export const ContactLable = styled('lable')({});
-export const ContactInput = styled('input')({
-  //   backgroundColor: 'rgba(104, 133, 176, 0.8)',
-  backgroundColor: 'transparent',
+export const ContactLable = styled('lable')({
+  color: 'rgba(255, 255, 255, 0.6)',
+  marginTop: '10px',
 });
-export const ContactAddBtn = styled('button')({});
+
+export const ContactInput = styled('input')({
+  backgroundColor: 'transparent',
+  border: 'none',
+  borderBottom: '2px solid rgba(255, 255, 255, 0.5)',
+  color: 'inherit',
+  font: 'inherit',
+  outline: 0,
+});
+
+const transition = '250ms cubic-bezier(0.4, 0, 0.2, 1)';
+
+export const ContactAddBtn = styled('button')({
+  marginTop: '20px',
+  cursor: 'pointer',
+  border: 'none',
+  color: 'rgba(145, 14, 14, 0.4)',
+  font: 'inherit',
+  backgroundColor: 'rgba(104, 133, 176, 0.4)',
+  boxShadow: '0px 0px 1px 1px rgba(0, 0, 0, 0.2)',
+  borderRadius: '10px',
+  height: '36px',
+
+  transition: `color ${transition}, box-shadow ${transition}`,
+
+  '&:hover': {
+    boxShadow: '0px 0px 1px 1px rgba(0, 0, 0, 0.4)',
+    color: 'rgba(145, 14, 14, 0.8)',
+  },
+});
