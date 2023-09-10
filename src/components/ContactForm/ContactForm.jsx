d72a6daebb4a7ae8ceab096faf96327a37834656
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-10 23:21:31 +0200

    contact form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -20,25 +20,21 @@ export class ContactForm extends Component {
       <PhonebookForm>
         <ContactLabel>Name</ContactLabel>
         <ContactInput
-        //   type="text"
-        //   name="name"
-        //   pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        //   title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        //   autoComplete="off"
-        //   value={this.state.name}
-        // onChange={this.getContactData}
-        //   required
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          autoComplete="off"
+          required
         />
         <ContactLabel>Number</ContactLabel>
         <ContactInput
-        //   type="tel"
-        //   name="number"
-        //   pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
-        //   title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-        //   autoComplete="off"
-        //   onChange={this.getContactData}
-        //   value={this.state.number}
-        //   required
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          autoComplete="off"
+          required
         />
         <ContactAddBtn>Add contact</ContactAddBtn>
       </PhonebookForm>
