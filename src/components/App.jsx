3f1001a190d06ed824d63fc59dabbaa63a5d52c5
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-10 20:09:06 +0200

    contact list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import { Container } from './index.styled';
+import { ContactList } from './ContactList/ContactList';
 
 export class App extends Component {
   // state = {
@@ -20,23 +21,40 @@ export class App extends Component {
   //   number: '',
   // };
 
-  // state = {
-  //   contacts: [
-  //     { id: 'id-1', name: 'Lesya Ukrainka', number: '459-12-56' },
-  //     { id: 'id-2', name: 'Boris JohnsonUK', number: '443-89-12' },
-  //     { id: 'id-3', name: 'Taras Shevchenko', number: '645-17-79' },
-  //     { id: 'id-4', name: 'Crimea BeachClub', number: '227-91-26' },
-  //   ],
-  //   filter: '',
-  // };
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Lesya Ukrainka', number: '459-12-56' },
+      { id: 'id-2', name: 'Boris JohnsonUK', number: '443-89-12' },
+      { id: 'id-3', name: 'Taras Shevchenko', number: '645-17-79' },
+      { id: 'id-4', name: 'Crimea BeachClub', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  getFilterContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase().trim())
+    );
+  };
+  removeContact = id => {
+    this.setState(prev => ({
+      contacts: prev.contacts.filter(contact => contact.id !== id),
+    }));
+  };
 
   render() {
+    const filterContacts = this.getFilterContacts();
     return (
       <Container>
         <h1>Phonebook</h1>
 
         <h2>Contacts</h2>
-        <p>some text</p>
+        <ContactList
+          contacts={filterContacts}
+          removeContact={this.removeContact}
+          getFilterContacts={this.getFilterContacts}
+        />
       </Container>
     );
   }
---
+++ src/components/ContactItem/ContactItem.styled.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+export const Contact = styled('li')({
+  display: 'flex',
+  height: '36px',
+  alignItems: 'center',
+  backgroundColor: 'rgba(104, 133, 176, 0.4)',
+  boxShadow: '0px 0px 1px 1px rgba(0, 0, 0, 0.2)',
+  borderRadius: '10px',
+  gap: '10px',
+  marginTop: '6px',
+  paddingRight: '8px',
+});
+
+export const ContactName = styled('p')({});
+export const ContactNumber = styled('p')({});
+
+const transition = '250ms cubic-bezier(0.4, 0, 0.2, 1)';
+
+export const ContactRemoveBtn = styled('button')({
+  marginLeft: 'auto',
+  cursor: 'pointer',
+  width: '22px',
+  height: '22px',
+  borderRadius: '100%',
+  backgroundColor: 'transparent',
+  border: 'transparent',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  transition: `fill ${transition}, box-shadow ${transition}, transform ${transition}`,
+
+  '& > svg': {
+    fontSize: '16px',
+    width: '16px',
+    height: '16px',
+    fill: 'rgba(145, 14, 14, 0.4)',
+  },
+
+  '&:hover': {
+    scale: '1.2',
+    boxShadow: '0px 0px 1px 1px rgba(0, 0, 0, 0.4)',
+  },
+
+  '&:hover svg': {
+    fill: '#bbb',
+    fill: 'rgba(145, 14, 14, 0.8)',
+    transition: `fill ${transition}, box-shadow ${transition}, transform ${transition}`,
+  },
+});
