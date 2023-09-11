45599b445173606d09bdb4c338292776684dd656
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-11 15:05:04 +0200

    contact item

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -12,8 +12,9 @@ export const ContactItem = ({ contacts, removeContact }) => {
         <ContactName>{name}:</ContactName>
         <ContactNumber>{number}</ContactNumber>
         <ContactRemoveBtn onClick={() => removeContact(id)}>
-          <svg viewBox="0 0 448 512">
-            <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
+          <svg id="icon-bin" viewBox="0 0 32 32">
+            <path d="M4 10v20c0 1.1 0.9 2 2 2h18c1.1 0 2-0.9 2-2v-20h-22zM10 28h-2v-14h2v14zM14 28h-2v-14h2v14zM18 28h-2v-14h2v14zM22 28h-2v-14h2v14z"></path>
+            <path d="M26.5 4h-6.5v-2.5c0-0.825-0.675-1.5-1.5-1.5h-7c-0.825 0-1.5 0.675-1.5 1.5v2.5h-6.5c-0.825 0-1.5 0.675-1.5 1.5v2.5h26v-2.5c0-0.825-0.675-1.5-1.5-1.5zM18 4h-6v-1.975h6v1.975z"></path>
           </svg>
         </ContactRemoveBtn>
       </Contact>
